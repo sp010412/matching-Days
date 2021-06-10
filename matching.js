fe8952277c7b398b0d02b0b33498db13c0a1be66
sp010412 <smajwede7@gmail.com>
2021-06-10 21:03:32 +0200

    complete

--- matching.js
+++ matching.js
@@ -1,14 +1,6 @@
 var dateOneElem = document.getElementById("dateOne");
 var dateTwoElem = document.getElementById("dateTwo");
 
-// var monElem = document.getElementById("Monday");
-// var tueElem = document.getElementById("Tuesday");
-// var wedElem = document.getElementById("Wednesday");
-// var thurElem = document.getElementById("Thursday");
-// var friElem = document.getElementById("Friday");
-// var satElem = document.getElementById("Saturday");
-// var sunElem = document.getElementById("Sunday");
-
 var dayI = document.querySelectorAll(".day")
 
 
@@ -27,22 +19,26 @@ dateOneElem.onchange = (e) => {
   e.preventDefault()
   var d = new Date(dateOneElem.value);
   var n = weekday[d.getDay()];
-  console.log(n)
 
-  for (var i = 0; i < dayI.length; i++) {
-    // console.log(weekday[dayI[i].value ])
-  }
+  dayI[d.getDay()].classList.remove("blue")
+
+
   if(dayI[d.getDay()]){
     dayI[d.getDay()].classList.add("orange")
   }
-
-  // console.log(dayI[i]);
 }
 
 dateTwoElem.onchange = () => {
   var d = new Date(dateTwoElem.value);
   var n = weekday[d.getDay()];
-  console.log(n)
+
+  dayI[d.getDay()].classList.remove("orange")
+
+  if(dayI[d.getDay()]){
+    dayI[d.getDay()].classList.add("blue")
+  }
+
 }
+
 
 
