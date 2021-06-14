95045c454f856632d6079c889d5172ed2359f228
sp010412 <smajwede7@gmail.com>
2021-06-14 13:05:32 +0200

    complete

--- factory.js
+++ factory.js
@@ -1,28 +1,32 @@
-// function dates() {
+function dates() {
 
-//     var arr = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"];
-//     var d = new Date();
-//     var day = new Array(7);
-//     day[0] = "Sunday";
-//     day[1] = "Monday";
-//     day[2] = "Tuesday";
-//     day[3] = "Wednesday";
-//     day[4] = "Thursday";
-//     day[5] = "Friday";
-//     day[6] = "Saturday";
+    var arr = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"];
 
-
-//     function find(firstD, secondD) {
-//         var date1 = new Date(firstD);
-//         var date2 = new Date(secondD);
-//         var aOne = day[date1.getDay()];
-//         var aTwo = day[date2.getDay()];
-//         return aOne && aTwo 
-//     }
+    var day = new Array(7);
+    day[0] = "Sunday";
+    day[1] = "Monday";
+    day[2] = "Tuesday";
+    day[3] = "Wednesday";
+    day[4] = "Thursday";
+    day[5] = "Friday";
+    day[6] = "Saturday";
 
 
 
-//     return {
-//         find,
-//     }
-// }
+    function find(firstD) {
+        var date1 = new Date(firstD);
+    return aOne = day[date1.getDay()];   
+    }
+
+    function findTwo(secondD) {
+    var date2 = new Date(secondD);
+    return aTwo = day[date2.getDay()];
+    }
+
+
+
+    return {
+        find,
+        findTwo,
+    }
+}
--- matching.js
+++ matching.js
@@ -4,40 +4,31 @@ var dateTwoElem = document.getElementById("dateTwo");
 var dayI = document.querySelectorAll(".day")
 
 
+var list = document.getElementsByTagName("li")
 
-
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-
-dateOneElem.onchange = (e) => {
-  e.preventDefault()
+dateOneElem.onchange = () => {
   var d = new Date(dateOneElem.value);
-  var n = weekday[d.getDay()];
+  var n = dayI[d.getDay()];
 
-  dayI[d.getDay()].classList.remove("blue")
-
-
-  if(dayI[d.getDay()]){
-    dayI[d.getDay()].classList.add("orange")
+  for(var i=0; i<list.length;i++){
+    var listIn = list[i];
+    listIn.classList.remove('orange')
   }
+
+  if (n) {
+    n.classList.add("orange");
+  }
+
 }
 
 dateTwoElem.onchange = () => {
-  var d = new Date(dateTwoElem.value);
-  var n = weekday[d.getDay()];
+  var c = new Date(dateTwoElem.value);
 
-  dayI[d.getDay()].classList.remove("orange")
+  var m = dayI[c.getDay()];
 
-  if(dayI[d.getDay()]){
-    dayI[d.getDay()].classList.add("blue")
+  if (m) {
+    m.classList.add("blue")
   }
-
 }
 
 
