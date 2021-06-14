6088e0523ea4b38ab97bb3d5f5f89a369ea5a7f8
sp010412 <smajwede7@gmail.com>
2021-06-14 15:33:52 +0200

    complete

--- matching.js
+++ matching.js
@@ -10,24 +10,41 @@ dateOneElem.onchange = () => {
   var d = new Date(dateOneElem.value);
   var n = dayI[d.getDay()];
 
+  var c = new Date(dateTwoElem.value);
+  var m = dayI[c.getDay()];
+
   for(var i=0; i<list.length;i++){
     var listIn = list[i];
     listIn.classList.remove('orange')
   }
 
   if (n) {
-    n.classList.add("orange");
+    n.classList.add("orange")
+  }
+
+  if(n === m){
+    n.classList.add("green");
+    m.classList.add("green");
   }
 
 }
 
 dateTwoElem.onchange = () => {
   var c = new Date(dateTwoElem.value);
-
   var m = dayI[c.getDay()];
 
-  if (m) {
-    m.classList.add("blue")
+  var d = new Date(dateOneElem.value);
+  var n = dayI[d.getDay()];
+
+  for(var i=0; i<list.length;i++){
+    var listIn = list[i];
+    listIn.classList.remove('blue')
+  }
+
+  
+  if(n === m){
+    n.classList.add("green");
+    m.classList.add("green");
   }
 }
 
